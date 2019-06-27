a497b6c9c561d288ce2bce245e4e105e3efde77a
Tristan Slater <hello@trslater.ca>
2019-06-27 02:07:43 -0700

    Finish basic hello world app

---
+++ hello-world.cpp
@@ -0,0 +1,13 @@
+/*
+ * Hello World
+ * -----------
+ *
+ * From the "Structure of a program" tutorial, found here: http://www.cplusplus.com/doc/tutorial/program_structure/
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!";
+}
